c13bc4d0eacb62857304a3570d1d92d8dda310a1
Nathaniel Ly <ntly03@cs.washington.edu>
2023-05-16 15:40:08 -0700

    Initial Implementation of QueryProcessor.cc

--- hw3/QueryProcessor.cc
+++ hw3/QueryProcessor.cc
@@ -16,6 +16,8 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <map>
+#include <unordered_set>
 
 extern "C" {
   #include "./libhw1/CSE333.h"
@@ -25,8 +27,16 @@ using std::list;
 using std::sort;
 using std::string;
 using std::vector;
+using std::map;
+using std::unordered_set;
 
 namespace hw3 {
+
+// Deletes pairs from the first map that do no intersect with the second map
+//
+// Returns true if returned map is non-empty; false otherwise
+static map<string, int> IntersectMaps(const map<string, int>& map1,
+                                      const map<string, int>& map2);
 
 QueryProcessor::QueryProcessor(const list<string>& index_list, bool validate) {
   // Stash away a copy of the index list.
@@ -79,11 +89,101 @@ QueryProcessor::ProcessQuery(const vector<string>& query) const {
   // STEP 1.
   // (the only step in this file)
   vector<QueryProcessor::QueryResult> final_result;
+  for (int curr_index = 0; curr_index < array_len_; curr_index++) {
+    DocTableReader* curr_dtr = dtr_array_[curr_index];
+    IndexTableReader* curr_itr = itr_array_[curr_index];
+    map<string, int> curr_index_result;
+    DocIDTableReader* didtr;
+    list<DocIDElementHeader> word_postings;
 
+    didtr = curr_itr->LookupWord(query[0]);
+
+    // Word not found, move onto next index
+    if (didtr == nullptr) {
+      delete didtr;
+      continue;
+    }
+
+    word_postings = didtr->GetDocIDList();
+    // Store file name and word count for each file
+    for (const auto& word_posting : word_postings) {
+      string file_name;
+      curr_dtr->LookupDocID(word_posting.doc_id, &file_name);
+      curr_index_result[file_name] = word_posting.num_positions;
+    }
+
+    delete didtr;
+
+    // If query is done, insert results into final_result
+    // and move onto next index
+    if (query.size() == 1) {
+      for (const auto& pair : curr_index_result) {
+        QueryResult res;
+        res.document_name = pair.first;
+        res.rank = pair.second;
+        final_result.push_back(res);
+      }
+      continue;
+    }
+
+    // Parse through the remaining words in the query
+    for (int i = 1; i < static_cast<int>(query.size()); i++) {
+      // Secondary map to store results for current word
+      map<string, int> curr_word_result;
+      didtr = curr_itr->LookupWord(query[i]);
+
+      // Word not found, move onto next index
+      if (didtr == nullptr) {
+        delete didtr;
+        curr_index_result.clear();
+        break;
+      }
+
+      // Store file name and word count for each file
+      word_postings = didtr->GetDocIDList();
+      for (const auto& word_posting : word_postings) {
+        string file_name;
+        curr_dtr->LookupDocID(word_posting.doc_id, &file_name);
+        curr_word_result[file_name] = word_posting.num_positions;
+      }
+      delete didtr;
+      // Find which files contain all proccessed words from the query
+      curr_index_result = IntersectMaps(curr_index_result, curr_word_result);
+      // Move onto next index if no files contain the query
+      if (curr_index_result.size() == 0) {
+        break;
+      }
+    }
+    // Insert results into final_result and move onto next index
+    if (curr_index_result.size() != 0) {
+      for (const auto& pair : curr_index_result) {
+        QueryResult res;
+        res.document_name = pair.first;
+        res.rank = pair.second;
+        final_result.push_back(res);
+      }
+    }
+  }
 
   // Sort the final results.
   sort(final_result.begin(), final_result.end());
   return final_result;
 }
 
+static map<string, int> IntersectMaps(const map<string, int>& map1,
+                                      const map<string, int>& map2) {
+  map<string, int> new_map;
+  for (const auto& pair : map1) {
+    string s = pair.first;
+    // If both maps contain the same key, combine their
+    // their values and insert into new map
+    if (map2.count(s) == 1) {
+      new_map[s] = pair.second + map2.at(s);
+    }
+  }
+  return new_map;
+}
+
+
 }  // namespace hw3
+
